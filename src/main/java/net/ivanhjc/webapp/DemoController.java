3078010466b1cfa0f872d84e6cce1b9df66ae9a0
Ivan Huang <hjcivan@gmail.com>
2022-04-01 20:25:45 +0800

    Init: A minimalist web app

---
+++ src/main/java/net/ivanhjc/webapp/DemoController.java
@@ -0,0 +1,32 @@
+package net.ivanhjc.webapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/")
+public class DemoController {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("greeting")
+    public String greeting(@RequestParam(name="name", defaultValue = "world") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping("helloworld")
+    @ResponseBody
+    public String helloWorld() {
+        String msg = "Hello World!";
+        log.info(msg);
+        log.error(msg);
+        return msg;
+    }
+}
